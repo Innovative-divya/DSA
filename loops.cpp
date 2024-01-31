78d519283cdf1e1c4d7ea80bfaa47c00fc44bd1d
Divyanshi <142373656+Innovative-divya@users.noreply.github.com>
2024-01-31 22:33:05 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=16>>1;
+    cout<<a;
+
+
+    // for(a=0;a<=10;a++){
+    //     if(a==5){
+    //     break;
+    //     }
+    // cout<<a <<std::endl;
+
+        
+    // };
+    // while(a<=10){
+    //     cout<<a<<std::endl;
+    //     a++;
+    // }
+    return 0;
+}
